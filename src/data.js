104728d0446e58fba808c896bc18e1dbd8cf30bd
adititiwariiiii <tripathi.aditi9990@gmail.com>
2022-08-27 19:55:11 +0530

    sample commit

---
+++ src/data.js
@@ -0,0 +1,75 @@
+export const sliderItems = [
+    {
+        id:1 , 
+        img: "https://i.pinimg.com/originals/6d/fc/3e/6dfc3e1858d1875ea0a62662ba8b7b36.png" ,
+        desc: "YOU LOOK INCOMPLETE:( GET 30% OFF ON ANY PIECE",
+        bg: "8677BA ",
+    },
+    {
+        id:2 , 
+        img: "https://i.pinimg.com/originals/b9/e4/8a/b9e48a83d90990a7a4930d88298c4334.png" ,
+        desc: "SUMMER COLLECTION! GET 30% OFF ON ANY PIECE",
+        bg: "584E7A ",
+    },
+    {
+        id:3 , 
+        img: "https://i.pinimg.com/originals/32/79/ab/3279ab13daf9f007e74913c9b4b24fb8.png" ,
+        desc: "SWEATER WEATHER<3 GET 30% OFF ON ANY PIECE",
+        bg: "2A263B",
+    }
+]
+
+export const categories = [
+    {
+      id: 1,
+      img: "https://afashionz.com/wp-content/uploads/2016/09/gold-ring-PNG.png",
+      title: "RINGS!",
+    },
+    {
+      id: 2,
+      img: "https://www.notandas.com/resources/images/home/gold-01.png",
+      title: "TRADITIONAL NECKLACEs",
+    },
+    {
+      id: 3,
+      img: "https://th.bing.com/th/id/R.d09041d666c016a3a30539f59f162a81?rik=aj1lwWCNznrJrA&riu=http%3a%2f%2fwww.joyas3d.com%2fwp-content%2fuploads%2f2020%2f06%2fbanner3.png&ehk=Nd3yeNepgsiZmzDiuuFiYVDaxlVT%2frpkXL1ACRrusYU%3d&risl=&pid=ImgRaw&r=0",
+      title: "BRACLETS",
+    },
+  ];
+
+
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://i.pinimg.com/originals/7e/78/78/7e7878266bdf99858b831f8234d25995.png",
+    },
+    {
+      id:2,
+      img:"https://i.pinimg.com/originals/db/bf/d3/dbbfd380d0f6be7a9b05676d1a6c9eb0.png",
+    },
+    {
+      id:3,
+      img:"https://i.pinimg.com/originals/4c/b7/79/4cb7797b8a0bfbf8c8cee9ecc0a90512.png",
+    },
+    {
+      id:4,
+      img:"https://th.bing.com/th/id/R.9c497d3b411e19cf8fd4f77eb547f1a2?rik=GIhas6RKIC60GQ&riu=http%3a%2f%2fwww.gcollinsandsons.com%2fimages%2fproducts%2f644-1.png&ehk=%2bB8Z7qwxiwOwWCHc2LlHUiK0Uk%2bPQerQ8a58DB2wtfk%3d&risl=&pid=ImgRaw&r=0",
+    },
+    {
+      id:5,
+      img:"https://th.bing.com/th/id/R.acd6c0b6542e06ded9fa65169322943b?rik=by9pZSMUB0c9fQ&riu=http%3a%2f%2fmikeseltzerjewelers.com%2fwp-content%2fuploads%2f2016%2f09%2fstackable-bracelets.png&ehk=%2b11Mv1Gg%2fMu6eoX4XhNKLgW%2bbSWDKovaNJ%2fmMjFZSl4%3d&risl=&pid=ImgRaw&r=0",
+    },
+    {
+      id:6,
+      img:"https://i.pinimg.com/originals/0a/50/02/0a50021a626e9e09d9f4338c8e86a6bb.png",
+    },
+    {
+      id:7,
+      img:"https://th.bing.com/th/id/R.9c3ec5dfda73b362dafa1e8f76fbe852?rik=XYX%2fFejzoPnkmA&riu=http%3a%2f%2f1.bp.blogspot.com%2f-QJfsC20pXho%2fUnp_vm0t7KI%2fAAAAAAAAAxw%2ffrOsYx2M-1E%2fs1600%2fgoldbeadsandpreciousstonesbridalset.png&ehk=%2bLjAtziPADFddJe2T0SF1Cn0m6ygUX3WLJ0yNqkdv6k%3d&risl=&pid=ImgRaw&r=0",
+    },
+    {
+      id:8,
+      img:"https://i.pinimg.com/originals/35/22/06/352206ff0e68170769e027c5d3fe78d1.png",
+    },
+  ]

dd16c109883f282182cc97b091da96e292a1d248
Aditi Tiwari <tripathi.aditi9990@gmail.com>
2023-02-11 20:41:13 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
+import Products from "./components/Products";
 
 const App = () => {
   return <Home/>;
